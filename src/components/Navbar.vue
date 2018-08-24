c538426d10a7189e99976dacf9fa79fcb9cff3b5
AleksC <aleksandar.cosic1@yahoo.com>
2018-08-24 20:53:01 +0200

    Initial commit + 2. zadatak

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <nav class="navbar-brand">
+            <router-link to="/">Home</router-link>
+        </nav>
+    </div>
+</template>
+<script>
+export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import TriviaApp from './components/TriviaApp'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: "/", component: TriviaApp }
+]
+
+const router = new VueRouter({ routes })
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
